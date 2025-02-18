73dad4fb076745384bea9dc808eb2ee50d82d921
吴玲玲 <211250113@smail.nju.edu.cn>
2025-02-18 15:04:39 +0800

    feat: download the two datasets

---
+++ Datasets/download.sh
@@ -0,0 +1,6 @@
+wget https://resources.mpi-inf.mpg.de/impact/dl_cred_analysis/Snopes.zip
+unzip Snopes.zip
+rm Snopes.zip
+wget https://resources.mpi-inf.mpg.de/impact/dl_cred_analysis/PolitiFact.zip
+unzip PolitiFact.zip
+rm PolitiFact.zip
