b8946d8a8cbdf88eab5e0a28cdaf39808e088351
吴玲玲 <211250113@smail.nju.edu.cn>
2025-03-10 20:31:54 +0800

    feat: update data preprocess

--- Datasets/preprocess.py
+++ Datasets/preprocess.py
@@ -6,24 +6,43 @@ import argparse
 def align_datasets(df1, df2):
     """
     Arguments:
-        df {pd.DataFrame}: the dataframe of Snopes/PolitiFact
+        df1 {pd.DataFrame}: the dataframe of Snopes
+        df2 {pd.Dataframe}: the dataframe of PolitiFact
     Returns:
         
     """
     # rearrange the order of columns
     # original order:
-    #     snope_df: <cred_label> <claim_source> <claim> <evidence> <evidence_source> 
-    #     politifact_df: <cred_label> <claim_id> <claim> <claim_source> <evidence> <evidence_source> 
+    #     df1: <cred_label> <claim_source> <claim> <evidence> <evidence_source> 
+    #     df2: <cred_label> <claim_id> <claim> <claim_source> <evidence> <evidence_source> 
     # rearranged order:
     #     <cred_label> <claim> <claim_source> <evidence> <evidence_source> 
-    if df.shape[1] == 5:
-        # Snopes
-        df = df[[0, 2, 1, 3, 4]]
-    else:
-        # PolitiFact
-        df = df[[0, 2, 3, 4, 5]]
-    df.columns = ['cred_label', 'claim', 'claim_source', 'evidence', 'evidence_source']
-    return df
+    df1 = df1[[0, 2, 1, 3, 4]]
+    df2 = df2[[0, 2, 3, 4, 5]]
+    df1.columns = ['cred_label', 'claim', 'claim_source', 'evidence', 'evidence_source']
+    df2.columns = ['cred_label', 'claim', 'claim_source', 'evidence', 'evidence_source']
+
+    df1['cred_label'] = df1['cred_label'].map({
+        'true': 'true',
+        'mostly true': 'true',
+        'false': 'false',
+        'mostly false': 'false'
+    })
+
+    df2['cred_label'] = df2['cred_label'].map({
+        'True': 'true', 
+        'Half-True': 'true', 
+        'Mostly True': 'true',
+        'False': 'false', 
+        'Pants on Fire!': 'false',
+        'Mostly False': 'false'
+    })
+
+    # limit evidence num of each claim less than 29
+    df2['rank'] = df2.groupby(['cred_label', 'claim', 'claim_source'])['evidence'].rank().astype('int')
+    df2 = df2[df2['rank'] < 29].drop(columns='rank')
+        
+    return df1, df2
 
 def df2json(df, file_path):
     """
@@ -46,7 +65,8 @@ def df2json(df, file_path):
         return
 
     f = open(file_path, 'w')
-    grouped = df.groupby(['cred_label', 'claim', 'claim_source'])
+    df['cred_label'] = df['cred_label'].astype('str')
+    grouped = df.groupby(['cred_label', 'claim_text', 'claim_source'])
     for (key, group) in grouped:
         group_dict = {
             'cred_label': key[0],
@@ -57,13 +77,3 @@ def df2json(df, file_path):
         group_json = json.dumps(group_dict)
         f.write(group_json + '\n')
     f.close()
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--infile')
-    parser.add_argument('--outfile')
-    args = parser.parse_args()
-    
-    df = pd.read_csv(args.infile, sep='\t', header=None)
-    df = align_datasets(df)
-    df2json(df, args.outfile)
